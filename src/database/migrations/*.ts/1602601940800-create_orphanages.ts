2f00ae601d77814e43addcb5e171a838503f4519
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-13 12:54:50 -0300

    Include opening_hours to the table

--- src/database/migrations/*.ts/1602601940800-create_orphanages.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class createOrphanages1602601940800 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
