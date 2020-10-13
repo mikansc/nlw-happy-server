5df5ef162fc9f30cde601ee08edd7c85d3fb9ade
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-13 13:35:29 -0300

    Add instructions column config

--- src/models/Orphanage.ts
+++ src/models/Orphanage.ts
@@ -2,11 +2,27 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity("orphanages")
 export class Orphanage {
-  @PrimaryGeneratedColumn("increment") id: number;
-  @Column() name: string;
-  @Column() latitude: number;
-  @Column() longitude: number;
-  @Column() about: string;
-  @Column() opening_hours: string;
-  @Column() open_on_weekends: boolean;
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  latitude: number;
+
+  @Column()
+  longitude: number;
+
+  @Column()
+  about: string;
+
+  @Column()
+  instructions: string;
+
+  @Column()
+  opening_hours: string;
+
+  @Column()
+  open_on_weekends: boolean;
 }
